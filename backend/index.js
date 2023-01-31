10063289b8b5f29724be220f20cf2306f70825ed
Abutalib Khan <thetalibkhan555@gmail.com>
2023-01-31 13:30:52 +0530

    database and server connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const app = express();
 
 //connect db
-
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URL, () =>
+  console.log("database is connected")
+);
 
 // start our server
+
+app.listen(process.env.PORT, () => console.log("server is connected"));
